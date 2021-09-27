45ddeec51ac8631521aaaebd1afcf37fb5b6bfa7
Dan Pock <self@danpock.me>
2021-09-27 10:00:43 -0400

    Adjust how HTML is rendered and remove from generator

--- resources/views/emoji.blade.php
+++ resources/views/emoji.blade.php
@@ -1,3 +1,5 @@
-<span {{ $attributes }}>
-    {{ $emoji }}
-</span>
+@if ($attributes !== null && (count($attributes->getIterator()) > 0 || $attributes->has('class')))
+<span {{ $attributes }}>{{ $emoji }}</span>
+@else
+<span>{{ $emoji }}</span>
+@endif
